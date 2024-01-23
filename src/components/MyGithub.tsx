7d88fc2b2e54339f4377eaaab45017c780590a1f
mcpdigital <internet@mcpdigital.com>
2024-01-23 00:16:26 -0300

    mygithub usestate

--- src/components/MyGithub.tsx
+++ src/components/MyGithub.tsx
@@ -3,8 +3,13 @@ import React from "react";
 import UserCard from "../components/ui/GHUserCard";
 import { GRAD_GOTHAN_TB } from "../tw_gradients";
 
+interface UserData {
+  id: number;
+  // Add other properties here
+}
+
 const FetchApi: React.FC = () => {
-  const [data, setData] = React.useState<any>(null);
+  const [data, setData] = React.useState<UserData | null>(null);
   const [error, setError] = React.useState<string | null>(null);
   const [loading, setLoading] = React.useState<boolean>(true);
 
@@ -35,7 +40,7 @@ const FetchApi: React.FC = () => {
     return <p>{error}</p>;
   }
 
-  if (!data) {
+  if (!data?.id) {
     return <p>No data found</p>;
   }
   // The key bellow is needed when maping multiple users REACT maps it, so since each user have an id, a single user can pass it too anyway
